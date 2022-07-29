a4bfed0763427c2b85af90a0e2260ba7b930db94
IceCang <107686384+IceCang@users.noreply.github.com>
2022-07-29 15:21:50 +0800

    Add files via upload

---
+++ main_app.py
@@ -0,0 +1,75 @@
+from distutils import extension
+from flask import Flask, render_template, session, request, jsonify
+from user_app import user_app
+import pymongo
+import markdown
+
+app = Flask(__name__)
+app.secret_key = 'h4qfibuardfautrr8tdd5i8v7ah72d'
+app.register_blueprint(user_app)
+
+client = pymongo.MongoClient("mongodb://localhost:27017")
+db = client['onlinejudge']
+db_web = db['web']
+
+@app.route('/')
+def welcome():
+    username = session.get('username')
+    if username is not None:
+        is_login = True
+    else:
+        is_login = False
+        username = '游客'
+    if user_is_banned():
+        return render_template('user/banned.html', t_username=username)
+    is_admin = False
+    if user_is_admin():
+        is_admin = True
+    notice = find_webdb({'key': 'notice'})
+    notice['html'] = markdown(
+        notice['content'], extensions=["fenced_code", "tables", "codehilite"]
+    )
+    return render_template('main/main.html', t_is_login=is_login, t_useravater=username, t_notice=notice['html'], t_is_admin=is_admin)
+#TODO add useravater("<img src=\"/static/user/avater/" + uid + ".png\" class=\"avater\">")
+
+@app.route('/notice_edit')
+def notice_edit():
+    return render_template('main/notice_edit.html')
+
+@app.route('/edit', methods=['POST'])
+def edit():
+    if request.form['type']=='notice':
+        if not user_is_admin():
+            return jsonify({'code':'1'})
+        db_web.update_one({'type': 'notice'}, {'$set': {'content': request.form['content']}})
+        return jsonify({'code':'1'})
+
+def user_is_banned():
+    username = session.get('username')
+    user_list = find_user({'username': username})
+    if len(user_list) == 0:
+        return False
+    if user_list[0]['state'] == 'banned':
+        return True
+    return False
+
+def user_is_admin():
+    username = session.get('username')
+    user_list = find_user({'username': username})
+    if len(user_list) == 0:
+        return False
+    if user_list[0]['state'] == 'admin':
+        return True
+    return False
+
+def find_user(condition):
+    c_user = db['user']
+    res = c_user.find(condition)
+    user_list = []
+    for item in res:
+        user_list.append(item)
+    return user_list
+
+def find_webdb(condition):
+    res = db_web.find(condition)
+    return res
---
+++ user_app.py
@@ -0,0 +1,159 @@
+from datetime import datetime
+from flask import Blueprint, render_template, request, redirect, session, jsonify
+import pymongo
+import hashlib
+
+user_app = Blueprint('user_app', __name__)
+
+client = pymongo.MongoClient("mongodb://localhost:27017")
+db_user = client['onlinejudge']
+c_user = db_user['user']
+c_online = db_user['online']
+c_last = db_user['last']
+
+
+@user_app.route('/register')
+def register():
+    return render_template('user/register.html')
+
+
+@user_app.route('/change_password')
+def change_password():
+    return render_template('user/change_password.html')
+
+
+@user_app.route('/register_check', methods=['POST'])
+def register_check():
+    username = request.form['username']
+    password = request.form['password']
+    banned_str="~`!@#$%^&*()=+{}[];:\'\"<>,.?\\|"
+    for i in banned_str:
+        if i in username:
+            return render_template('user/register.html', t_username=username, t_msg='用户名不能包含特殊字符！')
+    user_list = find_user({'username': username})
+    if len(user_list) == 0:
+        res = c_last.find()
+        last_list = []
+        for item in res:
+            last_list.append(item)
+        last_uid = last_list[0]['uid']
+        c_last.update_one({}, {"$set": {'uid': last_uid + 1}})
+        pwd = encrypt(password)
+        user = {'username': username, 'password': pwd, 'state': 'normal', 'uid': last_uid + 1}
+        insert_user(user)
+        return redirect('/login')
+    else:
+        return render_template('user/register.html', t_username=username, t_msg='用户名已经存在')
+
+
+@user_app.route('/change_submit', methods=['POST'])
+def change_submit():
+    username = session["username"]
+    password = request.form['old_password']
+    pwd = encrypt(password)
+    user_list = find_user({'username': username, 'password': pwd})
+    new_pwd = request.form['new_password']
+    new_pwd2 = request.form['new_password2']
+    if new_pwd != new_pwd2:
+        return render_template('user/change_password.html', t_msg='新密码错误')
+    elif len(user_list) == 1:
+        c_user.update_one({'username': username}, {"$set": {'password': encrypt(new_pwd)}})
+        session.pop('username')
+        return render_template('user/login.html', t_error='密码修改成功，请重新登录')
+    else:
+        return render_template('user/change_password.html', t_msg='原密码错误')
+
+
+'''
+    --------与登录有关的路由--------
+
+    /login           访问登录页面
+    /login_check     处理登录表单，实现登录功能
+
+'''
+
+
+
+@user_app.route('/login')
+def login():
+    return render_template('user/login.html')
+
+
+@user_app.route('/login_check', methods=['POST'])
+def login_check():
+    username = request.form['username']
+    pwd = request.form['password']
+    if pwd == None or username == None:
+        return jsonify({"message":"用户名或密码不能为空"})
+    user_list = find_user({'username': username, 'password': pwd})
+    if len(user_list) == 1:
+        # if user_list[0]['state'] == "2":
+        #     return render_template('user/login.html', t_error='用户信息待审核', t_color='2')
+        if user_list[0]['state'] != "banned":
+            session['username'] = username
+            if request.headers.getlist("X-Forwarded-For"):
+                ip = request.headers.getlist("X-Forwarded-For")[0]
+            else:
+                ip = request.remote_addr
+            c_user.update_one({'username': username}, {"$set": {'last_login_ip': ip}})
+            return jsonify({"code":"0"})
+        else:
+            return jsonify({"code":"2"})
+    else:
+        return jsonify({"code":"1"})
+
+@user_app.route('/banned')
+def banned():
+    return render_template("user/banned.html")
+
+@user_app.route('/logout')
+def logout():
+    session.pop('username')
+    return redirect('/')
+
+
+'''
+    --------与登录、注册有关的自定义函数--------
+
+    insert_user()     将注册用户数据存入数据库
+    find_user()       根据条件查找注册用户信息
+    encrypt()         对密码进行加密
+'''
+
+
+def insert_user(user):
+    c_user.insert_one(user)
+
+
+def find_user(condition):
+    res = c_user.find(condition)
+    user_list = []
+    for item in res:
+        user_list.append(item)
+    return user_list
+
+
+def encrypt(password):
+    pwd = hashlib.md5(password.encode(encoding='UTF-8')).hexdigest()
+    return pwd
+
+
+def check_login():
+    username = session.get('username')
+    if username is None:
+        return True
+
+
+def check_user():
+    username = session.get('username')
+    user_list = find_user({'username': username})
+    if len(user_list) == 0:
+        return False
+    if user_list[0]['state'] == 'banned':
+        return True
+    ts = datetime.timestamp(datetime.now())
+    if c_online.count_documents({'username': username}) != 0:
+        c_online.update_one({'username': username}, {"$set": {'time': ts}})
+    else:
+        c_online.insert_one({'username': username, 'time': ts})
+    return False
