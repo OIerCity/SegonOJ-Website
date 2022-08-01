66fd6955e0ad04244dfdbcdf78dd96b695c74067
IceCang <jason_lzm@163.com>
2022-08-01 15:48:48 +0800

    send_email!

--- user_app.py
+++ user_app.py
@@ -1,7 +1,24 @@
-from datetime import datetime
+from datetime import date, datetime
+from email import message
+import json
+from sqlite3 import Timestamp
 from flask import Blueprint, render_template, request, redirect, session, jsonify
 import pymongo
 import hashlib
+from email.mime.text import smtplib
+from email.header import MIMEText
+from email.header import Header
+from random import randint
+
+msg = '''
+<p>您好，您的SegonOJ注册链接是：</p>
+<p>https://segonoj.site/register_verify?email=
+'''
+esender = 'seojhelper2022@hotmail.com'
+epwd = 'M=GC/J=|)>uY}d_'
+
+smtpObj = smtplib.SMTP_SSL('smtp.office365.com', 587)
+smtpObj.login(esender, epwd)
 
 user_app = Blueprint('user_app', __name__)
 
@@ -10,26 +27,70 @@ db_user = client['onlinejudge']
 c_user = db_user['user']
 c_online = db_user['online']
 c_last = db_user['last']
+c_verify = db_user['email_verify']
 
 
 @user_app.route('/register')
 def register():
-    return render_template('user/register.html')
+    return render_template('user/register.html', t_found=False)
 
 
 @user_app.route('/change_password')
 def change_password():
     return render_template('user/change_password.html')
 
+@user_app.route('/register_send', methods=['POST'])
+def register_send():
+    email = request.form['email']
+    if len(find_user({'email': email})) == 1:
+        return jsonify({'code': '2'})
+    status, code = send_mail([email])
+    if status:
+        v_email = c_verify.findone({'email': email})
+        if v_email['used'] == 'yes':
+            return jsonify({'code': '-1'})
+        if v_email is not None:
+            if datetime.timestamp(datetime.now())-v_email['timestamp'] <= 60:
+                return jsonify({'code': '3'})
+            c_verify.update_one({'email': email}, {'$set': {'verify_code': code, 'passed': 'no'}})
+            return jsonify({'code': '0'})
+        else:
+            c_verify.insert_one({'email': email, 'verify_code': code, 'passed': 'no', 'used': 'no'})
+            return jsonify({'code': '0'})
+    else:
+        return jsonify({'code': '1'})
+
+@user_app.route('/register_verify')
+def register_verify():
+    email = request.args['email']
+    verify_code = request.args['code']
+    e_verify = c_verify.find_one({'email': email, 'code': verify_code})
+    if e_verify == None:
+        return render_template('user/register',t_found=False)
+    elif e_verify['used'] == 'yes':
+        return render_template('user/register',t_found=False,t_message='这邮箱被用过了')
+    else:
+        c_verify.update_one({'email': email, 'code': verify_code}, {'$set': {'passed': 'yes'}})
+        return render_template('user/register',t_found=True)
 
 @user_app.route('/register_check', methods=['POST'])
 def register_check():
     username = request.form['username']
     password = request.form['password']
-    banned_str="~`!@#$%^&*()=+{}[];:\'\"<>,.?\\|"
+    email = request.form['email']
+    banned_str = "~`!@#$%^&*()-=+{}[];:\'\"<>,.?\\|/"
+    e_verify = c_verify.find_one({'email': email, 'passed': 'yes', 'used': 'no'})
+    if e_verify is None:
+        return jsonify({'code': '1'})
     for i in banned_str:
         if i in username:
-            return render_template('user/register.html', t_username=username, t_msg='用户名不能包含特殊字符！')
+            return jsonify({'code': '2'})
+    # username = request.form['username']
+    # password = request.form['password']
+    # banned_str="~`!@#$%^&*()=+{}[];:\'\"<>,.?\\|"
+    # for i in banned_str:
+    #     if i in username:
+    #         return render_template('user/register.html', t_username=username, t_msg='用户名不能包含特殊字符！')
     user_list = find_user({'username': username})
     if len(user_list) == 0:
         res = c_last.find()
@@ -39,11 +100,12 @@ def register_check():
         last_uid = last_list[0]['uid']
         c_last.update_one({}, {"$set": {'uid': last_uid + 1}})
         pwd = encrypt(password)
-        user = {'username': username, 'password': pwd, 'state': 'normal', 'uid': last_uid + 1}
+        user = {'username': username, 'password': pwd, 'state': 'normal', 'uid': last_uid + 1, 'email': email}
         insert_user(user)
-        return redirect('/login')
+        return jsonify({'code': '1'})
     else:
-        return render_template('user/register.html', t_username=username, t_msg='用户名已经存在')
+        return jsonify({'code': '2'})
+    
 
 
 @user_app.route('/change_submit', methods=['POST'])
@@ -118,6 +180,7 @@ def logout():
     insert_user()     将注册用户数据存入数据库
     find_user()       根据条件查找注册用户信息
     encrypt()         对密码进行加密
+    send_mail()       对邮箱发送邮件
 '''
 
 
@@ -157,3 +220,24 @@ def check_user():
     else:
         c_online.insert_one({'username': username, 'time': ts})
     return False
+
+def send_mail(ereciever):
+    try:
+        code = randint(1000000,9999999)
+        emsg = MIMEText(msg + ereciever[0] + '''&code=''' + str(code) + '''</p>''', 'html', 'utf-8')
+        emsg['From'] = Header("SegonOJ", 'utf-8')
+        emsg['To'] = Header("注册邮箱", 'utf-8')
+        emsg['Subject'] = Header('SegonOJ - 注册邮箱验证', 'utf-8')
+        smtpObj.sendmail(esender, ereciever, emsg.as_string())
+        return True, str(code)
+    except:
+        return False, str(code)
+
+'''
+    --------用户个人页面--------
+'''
+
+@user_app.route('/user/<int:uid>')
+def user_page():
+    return render_template('user/selfpage.html')
+
