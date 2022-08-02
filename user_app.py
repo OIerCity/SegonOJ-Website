faf18c36198a76ee2d4219c1067c3cccc64c09a4
IceCang <jason_lzm@163.com>
2022-08-02 20:37:30 +0800

    OHHHHHH

--- user_app.py
+++ user_app.py
@@ -18,8 +18,8 @@ esender = 'seojhelper@hotmail.com'
 epwd = 'M=GC/J=|)>uY}d_'
 
 smtpObj = smtplib.SMTP('smtp.office365.com', 587)
-smtpObj = smtplib.ehlo()
-smtpObj = smtplib.starttls()
+smtpObj.ehlo()
+smtpObj.starttls()
 smtpObj.login(esender, epwd)
 
 user_app = Blueprint('user_app', __name__)
