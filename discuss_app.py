8e10889f765a1b1578def1a8707d8d79031449ab
IceCang <jason_lzm@163.com>
2022-08-21 09:36:26 +0800

    discuss test

--- discuss_app.py
+++ discuss_app.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, session, jsonify
+from flask_pagination import Pagination
 import pymongo
+import user_app
 
 
 discuss_app = Blueprint('discuss_app', __name__)
@@ -8,14 +10,45 @@ client = pymongo.MongoClient("mongodb://localhost:27017")
 db_discuss = client['onlinejudge']
 c_discuss = db_discuss['discuss']
 
+per_page = 20
+page_limit = 2000
+
+@discuss_app.before_request
+def before_request():
+    if user_app.check_login():
+        return render_template('user/login.html')
+    if user_app.check_user():
+        username = session.get('username')
+        return render_template('user/banned.html', t_username=username)
+
 @discuss_app.route('/discuss/list')
 def discuss_list():
+    username = session.get('username')
+    user = db_discuss['user'].find_one({'username': username})
+    userhavebadge = False
+    if user['have_badge']:
+        userhavebadge = True
+    if user['status'] == 'admin':
+        is_admin = True
+    condition = {'status': 'public'}
+    
     if request.args['forumname'] == None:
-        c_discuss
-        return
+        if request.args['page'] == None:
+            page = 0
+        else: page = request.args['page']
+        alist = find_discuss(condition, page)
+        pagination = Pagination(page=page, per_page=per_page, total=max(c_discuss.count_documents(condition),page_limit), search=False, record_name='alist')
+        return render_template('discuss/list.html', t_is_admin=is_admin, t_userhavebadge=userhavebadge, t_userbadge=user['badge'], t_usercolor=user['color'], t_username=username, t_discuss_list=alist)
     else:
         return
 
 @discuss_app.route('/discuss/<int:id>')
 def discuss_view(id):
     return
+
+def find_discuss(condition,page = 0):
+    if page == 0:
+        res = c_discuss.find(condition).sort([{"sticky",pymongo.DESCENDING}, {"date", pymongo.DESCENDING}]).limit(20)
+    else:
+        res = c_discuss.find(condition).sort({"date",pymongo.DESCENDING}).skip((page-1)*per_page).limit(per_page)
+    return res
