51e840f9d01bb2dce178ecca29ee62afd2ab55ef
IceCang <jason_lzm@163.com>
2022-08-21 10:18:13 +0800

    plzzzzz

--- discuss_app.py
+++ discuss_app.py
@@ -48,7 +48,7 @@ def discuss_view(id):
 
 def find_discuss(condition,page = 0):
     if page == 0:
-        res = c_discuss.find(condition).sort([{"sticky",pymongo.DESCENDING}, {"date", pymongo.DESCENDING}]).limit(20)
+        res = c_discuss.find(condition).sort([("sticky",pymongo.DESCENDING), ("date", pymongo.DESCENDING)]).limit(20)
     else:
         res = c_discuss.find(condition).sort({"date",pymongo.DESCENDING}).skip((page-1)*per_page).limit(per_page)
     return res
