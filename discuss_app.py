6274b45e3f5f6b718b9818fb93482dbaee2df499
IceCang <jason_lzm@163.com>
2022-08-22 21:19:01 +0800

    debug

--- discuss_app.py
+++ discuss_app.py
@@ -44,7 +44,7 @@ def discuss_list():
             lastcommenter = item['lastcommenter']
             item['owner'] = c_user.find_one({'uid':owner})
             item['lastcommenter'] = c_user.find_one({'uid':lastcommenter})
-        return render_template('discuss/list.html',t_is_login=True, t_is_admin=is_admin, t_userhavebadge=userhavebadge, t_userbadge=user['badge'], t_usercolor=user['color'], t_username=username, t_discuss_list=discuss_list)
+        return render_template('discuss/list.html',t_is_login=True, t_is_admin=is_admin, t_userhavebadge=userhavebadge, t_userbadge=user['userbadge'], t_usercolor=user['color'], t_username=username, t_discuss_list=discuss_list)
     else:
         return
 
